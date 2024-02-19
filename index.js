0465382997d1dad3d2dd219c188144df5da6a4ce
Xaheersays <shzaheer514@gmail.com>
2024-02-19 12:32:49 +0530

    reset

--- index.js
+++ index.js
@@ -1,53 +1,54 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
-const cors = require('cors')
+const puppeteer = require('puppeteer-core'); // Use puppeteer-core
+const cors = require('cors');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const chromium = require('@sparticuz/chromium');
 
 app.use(cors({
   origin: '*'
 }));
-
-
-
 
 app.get('/search/:keyword', async (req, res) => {
   const keyword = req.params.keyword;
   const products = await scrapeAmazon(keyword);
   res.json(products);
 });
-async function scrapeAmazon(keyword) {
-  const browser = await puppeteer.launch(
 
-  );
+async function scrapeAmazon(keyword) {
+  const executablePath = await chromium.executablePath(); // Get the path to Chromium binary
+
+  const browser = await puppeteer.launch({
+    executablePath, // Specify the path to Chromium binary
+    args: chromium.args,
+    defaultViewport: chromium.defaultViewport,
+    headless: chromium.headless
+  });
+
   const page = await browser.newPage();
-  
+
   await page.goto(`https://www.amazon.com/s?k=${keyword}`);
-  
+
   const products = await page.evaluate(() => {
     const ratingPattern = /(\d+(\.\d+)?) out of \d+ stars/;
     const results = [];
     const items = document.querySelectorAll("[data-component-type='s-search-result']");
     items.forEach(item => {
       const name = (item.querySelector('h2')?.innerText || "").trim();
-
       const description = (item.querySelector("[data-cy='title-recipe']").getElementsByTagName('span')[1]?.innerText || "").trim();
-
       const textR = item.querySelector('.a-popover-trigger')?.querySelector('span')?.innerText || ""
       const match = textR.match(ratingPattern);
       const rating = match ? match[1] : "No rating found";
-      
       const reviewCount = item.querySelector('.a-size-base.s-underline-text')?.innerText
       const price = (item.querySelector('.a-price span:last-child')?.innerText?.replace(/\n/g, '') || "not found")
       results.push({ name, description, rating, reviewCount, price });
     });
     return results.slice(0, 4);
   });
-  
+
   await browser.close();
   return products;
 }
-
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
--- pp2/divine-union-b6c8/src/index.js
+++ pp2/divine-union-b6c8/src/index.js
@@ -3,32 +3,43 @@ import puppeteer from "@cloudflare/puppeteer";
 export default {
 	async fetch(request, env) {
 		const { searchParams } = new URL(request.url);
-		let url = searchParams.get("url");
-		let img;
-		if (url) {
-			url = new URL(url).toString(); // normalize
-			img = await env.BROWSER_KV_DEMO.get(url, { type: "arrayBuffer" });
-			if (img === null) {
-				const browser = await puppeteer.launch(env.MYBROWSER);
-				const page = await browser.newPage();
-				await page.goto(url);
-				img = await page.screenshot();
-				await env.BROWSER_KV_DEMO.put(url, img, {
-					expirationTtl: 60 * 60 * 24,
-				});
-				await browser.close();
-			}
-			return new Response(img, {
+		let keyword = searchParams.get("keyword");
+
+		if (keyword) {
+			const products = await scrapeAmazon(keyword, env);
+			return new Response(JSON.stringify(products), {
 				headers: {
-					"content-type": "image/jpeg",
+					"content-type": "application/json",
 				},
 			});
 		} else {
-			return new Response(
-				
-				"Please add an ?url=https://example.com/ parameter"
-				
-			);
+			return new Response("Please add a ?keyword=yourKeyword parameter");
 		}
 	},
 };
+
+async function scrapeAmazon(keyword, env) {
+	const browser = await puppeteer.launch(env.MYBROWSER);
+	const page = await browser.newPage();
+	await page.goto(`https://www.amazon.com/s?k=${keyword}`);
+
+	const products = await page.evaluate(() => {
+		const ratingPattern = /(\d+(\.\d+)?) out of \d+ stars/;
+		const results = [];
+		const items = document.querySelectorAll("[data-component-type='s-search-result']");
+		items.forEach((item) => {
+			const name = (item.querySelector('h2')?.innerText || "").trim();
+			const description = (item.querySelector("[data-cy='title-recipe']").getElementsByTagName('span')[1]?.innerText || "").trim();
+			const textR = item.querySelector('.a-popover-trigger')?.querySelector('span')?.innerText || "";
+			const match = textR.match(ratingPattern);
+			const rating = match ? match[1] : "No rating found";
+			const reviewCount = item.querySelector('.a-size-base.s-underline-text')?.innerText;
+			const price = (item.querySelector('.a-price span:last-child')?.innerText?.replace(/\n/g, '') || "not found");
+			results.push({ name, description, rating, reviewCount, price });
+		});
+		return results.slice(0, 4);
+	});
+
+	await browser.close();
+	return products;
+}
